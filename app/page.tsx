7f4c5116c3ec97e89c90513920ee303d2e0f3208
Caligo Rocha Brasil <caligobrasileiro@hotmail.com>
2026-09-13 23:24:32 -0300

    Adicionando o requirements.txt para o deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'; 
-import { useState, useEffect } from 'react';
-import './globals.css'; 
+import { useEffect, useState } from 'react';
+import './globals.css';
 
 export default function Home() {
   // =====================================================================
